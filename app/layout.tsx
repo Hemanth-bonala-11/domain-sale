dcb34df99c8a8b3e803a257d3db6be17ec778655
namekart-hemanth <t-hemanth@namekart.com>
2025-03-21 15:16:20 +0530

    added google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { ThemeProvider } from '@/components/theme-provider';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Domain For Sale | Premium Domain Name',
-  description: 'This premium domain name is available for purchase. Contact us to make an offer.',
+  title: "Domain For Sale | Premium Domain Name",
+  description: "This premium domain name is available for purchase. Contact us to make an offer.",
 };
 
 export default function RootLayout({
@@ -17,6 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+      <Script
+          strategy="afterInteractive"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3408432257318389"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-3408432257318389" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
